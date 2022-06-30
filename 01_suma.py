3b46305186a4d8737acab2f91f4473c4a19ef7d5
Paola Dev <paola.cartala@gmail.com>
2022-06-30 09:21:23 -0300

    WIP: modifica suma

--- 01_suma.py
+++ 01_suma.py
@@ -1,8 +1,13 @@
 from enteros import ing2i
 
-def suma():
-  n1, n2 = ing2i()
-  sumar = n1 + n2
-  return sumar
-  
-print(f'La suma es: {suma()}')
+n1, n2 = ing2i()
+def suma(n1, n2):
+    """suma dos enteros ingresados por el usuario y retorna el resultado
+
+    Returns:
+        int: suma de los enteros ingresados por el usuario
+    """
+    sumar = n1 + n2
+    return sumar
+
+print(f'La suma es: {suma(n1, n2)}')
